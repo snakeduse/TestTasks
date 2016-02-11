47b41df7d082b210b1d707c9e15bafe2ef9f0e69
Aleksey Profatilov <snakeduse@gmail.com>
2016-02-12 00:29:44 +0300

    начал делать болванку для репозитория

---
+++ Tasks/Repositories/Contacts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Repositories.Contacts
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        // все методы реализовывать не стал, потому что нет надобности
+
+        void Add(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ Tasks/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Tasks.Repositories.Contacts;
+
+namespace Tasks.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext DatabaseContext;
+
+        public Repository(DbContext databaseContext)
+        {
+            DatabaseContext = databaseContext;
+        }
+
+        public void Add(TEntity entity)
+        {
+            DatabaseContext.Set<TEntity>().Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            DatabaseContext.Set<TEntity>().Remove(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return DatabaseContext.Set<TEntity>().ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
